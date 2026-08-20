121427476f05788676113f0205570acc9e9d26d9
WINBD-PRO <winbdproofficial@gmail.com>
2026-08-21 04:27:04 +0600

    Bust cache after splash and Telegram fixes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='winbd-pro-v3';
+const CACHE='winbd-pro-v4';
 const APP_SHELL=['/','/login.html','/register.html','/account.html','/manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
